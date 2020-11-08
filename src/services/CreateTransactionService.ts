cf619482392a00429ec7d1217899ed3adcaa492a
Joao Albuquerque <joaohcca@gmail.com>
2020-11-08 11:48:54 -0300

    adicionando resoluçao desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: "income" | "outcome";
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,14 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type}: Request): Transaction {
+    //check balance to add at the end
+    const transaction = this.transactionsRepository.create({title, value, type})
+    const {total} = this.transactionsRepository.getBalance()
+    if (type == 'outcome' && value > total){
+      throw new Error ('not enough balance')
+    }
+    return transaction; 
   }
 }
 
